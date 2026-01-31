8a48ba289df0eb6e18ffc9b47bfdd7cb2ae9ef12
Shantani <shantanisinha07@gmail.com>
2026-01-31 12:07:58 -0500

    Update ShooterSubsystem.java

--- Little tiger/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ Little tiger/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -1,10 +1,13 @@
 package frc.robot.subsystems;
-
 import java.util.function.DoubleSupplier;
-
+import static edu.wpi.first.wpilibj2.command.Commands.parallel;
+import static edu.wpi.first.wpilibj2.command.Commands.waitUntil;
+import edu.wpi.first.wpilibj.Encoder;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
-
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.examples.rapidreactcommandbot.Constants.ShooterConstants;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
@@ -14,14 +17,54 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class ShooterSubsystem extends SubsystemBase
 {
     private SparkMax Shootermotor = new SparkMax (10, MotorType.kBrushless);
-
+    private final Encoder m_shooterEncoder = 
+        new Encoder(
+            ShooterConstants.kEncoderPorts[0], 
+            ShooterConstants.kEncoderPorts[1],
+            ShooterConstants.kEncoderReversed);
+    private final SimpleMotorFeedforward m_shooterFeedforward =
+        new SimpleMotorFeedforward(
+            ShooterConstants.kSVolts,
+            ShooterConstants.kVVoltSecondsPerRotation);
+    private final PIDController m_ShooterFeedback =
+        new PIDController(
+            ShooterConstants.kP,
+            ShooterConstants.kI,
+            ShooterConstants.kD);
     public ShooterSubsystem()
     {
-        
-    }
+    m_ShooterFeedback.setTolerance(ShooterConstants.kShooterToleranceRPS);
+    m_shooterEncoder.setDistancePerPulse(ShooterConstants.kEncoderDistancePerPulse);
+        // Set default command to turn off both the shooter and feeder motors, and then idle
+
+    setDefaultCommand(
+        runOnce(() -> {
+            m_ShooterMotor.disable();
+            m_feederMotor.disable();
+        }) 
+          .andThen(run(() -> {}))
+          .withName("Idle"));
+  }
+    
+  /**
+   * Returns a command to shoot the balls currently stored in the robot. Spins the shooter flywheel
+   * up to the specified setpoint, and then runs the feeder motor.
+   *
+   * @param setpointRotationsPerSecond The desired shooter velocity
+   */
 
     public Command runShootCommand(DoubleSupplier power)
-    {
-        return this.startEnd(() -> Shootermotor.set(power.getAsDouble()), () -> Shootermotor.set(0));
+    {   
+        run_parallel(
+            run(
+                () -> {
+                    m_ShooterMotor.set(m_shooterFeedforward.calculate(setpointRotationsPerSecond) +
+                    m_ShooterFeedback.calculate(m_shooterEncoder.getRate(), setpointRotationsPerSecond));
+                }
+        )
+        // Wait until the shooter has reached the setpoint, and then run the feeder
+            waitUntil(m_shooterFeedback::atSetpoint).andThen(() -> m_feederMotor.set(1)))
+        .withName("Shoot");
+        // return this.startEnd(() -> Shootermotor.set(power.getAsDouble()), () -> Shootermotor.set(0));
     }
 }
